41e8da940907b842dfa453083dd62be662e8b60c
이호연 <ho991217@kakao.com>
2024-03-03 17:52:59 +0900

    chore: 기존 input 컴포넌트 제거

--- app/components/common/input.tsx
+++
@@ -1,9 +0,0 @@
-import { HTMLAttributes, HTMLInputTypeAttribute } from 'react';
-
-type InputProps = HTMLAttributes<HTMLInputElement> & {
-  type?: HTMLInputTypeAttribute;
-};
-
-export default function Input(props: InputProps) {
-  return <input {...props} />;
-}
