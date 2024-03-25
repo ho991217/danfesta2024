49ab7a60f6dbc566b8e6c01337241efb5ecbacb8
이호연 <ho991217@kakao.com>
2024-03-25 10:11:41 +0900

    feat: ios인 경우 구글 크롬으로 이동하도록 변경

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,30 @@
 import createMiddleware from 'next-intl/middleware';
+import { permanentRedirect, redirect } from 'next/navigation';
+import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-export default createMiddleware({
+const i12nMiddleware = createMiddleware({
   locales: ['en', 'ko'],
   defaultLocale: 'ko',
   localeDetection: true,
 });
 
+const middleware = (request: NextRequest): NextResponse<unknown> => {
+  const url = request.nextUrl;
+  const { os, browser } = userAgent(request);
+  if (browser.name === 'KAKAOTALK' || browser.name === 'NAVER') {
+    if (os.name === 'Android') {
+      permanentRedirect(`intent://${url.toString()}#Intent;scheme=http;end';`);
+    }
+    if (os.name === 'iOS') {
+      permanentRedirect(`googlechrome://${url.toString()}`);
+    }
+  }
+
+  return i12nMiddleware(request);
+};
+
 export const config = {
   matcher: ['/', '/(ko|en)/:path*'],
 };
+
+export default middleware;
