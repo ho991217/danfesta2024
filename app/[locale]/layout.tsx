1e4d857ceba9977bb9a86243b2234aed5e9cf73c
이호연 <ho991217@kakao.com>
2024-03-25 09:37:48 +0900

    feat: 모바일 환경 권장 문구 추가

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -18,8 +18,11 @@ export default function LocaleLayout({
     <html lang={locale}>
       <body className={clsx(NotoSansKR.className, 'scrollbar-hide')}>
         <Providers>
-          <div className='max-w-[600px] min-w-[320px] m-auto px-5 pt-5 h-[100dvh] overflow-x-hidden'>
+          <div className='max-w-[600px] min-w-[320px] m-auto px-5 pt-5 h-[100dvh] overflow-x-hidden lg:hidden'>
             {children}
+          </div>
+          <div className='hidden lg:block lg:fixed lg:top-1/2 lg:left-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2'>
+            모바일 환경에서 접속해주세요.
           </div>
         </Providers>
       </body>
