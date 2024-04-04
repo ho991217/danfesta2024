ba34d427db6a22f15e7247f86aa21d72140e912a
이호연 <ho991217@kakao.com>
2024-04-04 13:39:03 +0900

    fix: 티켓신청 수정

--- components/ticketing/form.tsx
+++ components/ticketing/form.tsx
@@ -10,6 +10,8 @@ import APIError from '@/lib/utils/error/api-error';
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
 import { useLocale } from 'next-intl';
+import { API_ROUTES, API_URL, COOKIE_KEYS } from '@/constants';
+import { useCookies } from 'next-client-cookies';
 
 const schema = z.object({
   captchaValue: z.string().trim().min(1, { message: '필수 입력 사항입니다.' }),
@@ -23,13 +25,24 @@ export default function TicketingForm({
 }: Omit<TicketApplyRequest, 'captchaValue'>) {
   const router = useRouter();
   const locale = useLocale();
+  const cookies = useCookies();
 
   const handleSubmit = async (v: Schema) => {
+    const atk = cookies.get(COOKIE_KEYS.accessToken);
+    if (!atk) throw new Error('로그인이 필요합니다.');
+
     try {
-      await applyTicket({
-        eventId,
-        captchaKey,
-        captchaValue: v.captchaValue,
+      await fetch(`${API_URL}${API_ROUTES.ticket.apply}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${atk}`,
+        },
+        body: JSON.stringify({
+          eventId,
+          captchaKey,
+          captchaValue: v.captchaValue,
+        }),
       });
       toast.success('신청이 완료되었습니다.');
       router.push(`/${locale}/ticketing/${eventId}/result`);
