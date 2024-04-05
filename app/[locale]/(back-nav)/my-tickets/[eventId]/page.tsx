fcf38fbdc7cab9f0352ee75c778ebeed45ae0e8c
이호연 <ho991217@kakao.com>
2024-04-05 10:50:03 +0900

    feat: 데이터가 없는 경우 처리

--- app/[locale]/(back-nav)/my-tickets/[eventId]/page.tsx
+++ app/[locale]/(back-nav)/my-tickets/[eventId]/page.tsx
@@ -1,30 +1,26 @@
 import Ticket from '@/components/my-tickets/ticket';
 import { getMyTicket } from '../action';
-import If from '@/components/util/if';
 
 export default async function Detail({
   params: { eventId },
 }: {
   params: { eventId: number };
 }) {
-  let ticketId;
   try {
     const { id } = await getMyTicket(eventId);
-    ticketId = id;
-  } catch (e) {
-    ticketId = null;
-  }
 
-  return (
-    <section className='flex flex-col px-5 pt-5 w-full h-[calc(100dvh-100px)] bg-primary items-center'>
-      <If condition={!!ticketId}>
-        <If.Then>{ticketId && <Ticket ticketId={ticketId} />}</If.Then>
-        <If.Else>
-          <span className='text-neutral-400 dark:text-neutral-600'>
-            티켓이 없습니다.
-          </span>
-        </If.Else>
-      </If>
-    </section>
-  );
+    return (
+      <section className='flex flex-col px-5 pt-5 w-full h-[calc(100dvh-100px)] bg-primary items-center'>
+        <Ticket ticketId={id} />
+      </section>
+    );
+  } catch (e) {
+    const err = e as Error;
+
+    return (
+      <section className='flex flex-col px-5 pt-5 w-full h-[calc(100dvh-100px)] items-center text-neutral-400 dark:text-neutral-800'>
+        {err.message}
+      </section>
+    );
+  }
 }
