3b120a0be76f51940730a7fffd02ad67035c661d
이호연 <ho991217@kakao.com>
2024-04-09 10:22:17 +0900

    xpt

--- components/home/lineup-tile.tsx
+++ components/home/lineup-tile.tsx
@@ -5,31 +5,37 @@ import { LineupInfo } from '@/app/[locale]/(back-nav)/lineup/page';
 import { getTranslations } from 'next-intl/server';
 
 export default async function LineupTile() {
-  const allDay = ['FIRST_DAY', 'SECOND_DAY', 'THIRD_DAY'] as const;
-  const data = (
-    await Promise.all(
-      allDay.map((day) =>
-        fetch(`${API_URL}${API_ROUTES.lineup.list(day)}`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          cache: 'no-store',
-        }).then((res) => res.json() as Promise<LineupInfo[]>)
+  try {
+    const allDay = ['FIRST_DAY', 'SECOND_DAY', 'THIRD_DAY'] as const;
+    const data = (
+      await Promise.all(
+        allDay.map((day) =>
+          fetch(`${API_URL}${API_ROUTES.lineup.list(day)}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            cache: 'no-store',
+          }).then((res) => res.json() as Promise<LineupInfo[]>)
+        )
       )
-    )
-  ).flat();
-  const t = await getTranslations('LineupTile');
+    ).flat();
+    const t = await getTranslations('LineupTile');
 
-  return (
-    <div className='w-full'>
-      <TileHeader>
-        <TileHeader.Head>{t('title')}</TileHeader.Head>
-        <TileHeader.SeeAll href='/lineup'>{t('seeAll')}</TileHeader.SeeAll>
-      </TileHeader>
-      <div className='w-full aspect-[3/4] relative'>
-        <Carousel images={data} />
+    return (
+      <div className='w-full'>
+        <TileHeader>
+          <TileHeader.Head>{t('title')}</TileHeader.Head>
+          <TileHeader.SeeAll href='/lineup'>{t('seeAll')}</TileHeader.SeeAll>
+        </TileHeader>
+        <div className='w-full aspect-[3/4] relative'>
+          <Carousel images={data} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    const e = error as Error;
+    console.error(error);
+    return <span className='w-full text-neutral-500'>{e.message}</span>;
+  }
 }
