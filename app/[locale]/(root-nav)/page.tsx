302f6a1da000b709502b553f4ea0239ac7bd8c91
이호연 <ho991217@kakao.com>
2024-04-09 10:20:37 +0900

    ㄷㅈㄹ

--- app/[locale]/(root-nav)/page.tsx
+++ app/[locale]/(root-nav)/page.tsx
@@ -3,7 +3,7 @@ import { TicketTile, MenuTiles, LineupTile } from '@/components/home';
 export default function Home() {
   return (
     <div className='flex flex-col gap-4 mb-20 px-5'>
-      <LineupTile />
+      {/* <LineupTile /> */}
       <TicketTile />
       <MenuTiles />
     </div>
