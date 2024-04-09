acfcedf3c6ce1fba49d3a97ab42ecf9606273590
이호연 <ho991217@kakao.com>
2024-04-09 14:21:29 +0900

    아이유

--- components/home/lineup-tile.tsx
+++ components/home/lineup-tile.tsx
@@ -1,47 +1,34 @@
 import Carousel from "../common/carousel";
 import TileHeader from "./tile-header";
-import { API_ROUTES, API_URL } from "@/constants";
 import { LineupInfo } from "@/app/[locale]/(back-nav)/lineup/page";
 import { getTranslations } from "next-intl/server";
 
 export default async function LineupTile() {
   try {
     const allDay = ["FIRST_DAY", "SECOND_DAY", "THIRD_DAY"] as const;
-    // const data = await Promise.all(
-    //   allDay
-    //     .map((day) =>
-    //       fetch(`${API_URL}${API_ROUTES.lineup.list(day)}`, {
-    //         method: 'GET',
-    //         headers: {
-    //           'Content-Type': 'application/json',
-    //         },
-    //         cache: 'no-store',
-    //       }).then((res) => res.json())
-    //     )
-    //     .flat()
-    // );
+    const data = await Promise.all(
+      allDay.map((day) =>
+        fetch(`https://next.danvery.com/api/line-up?festivalDate=${day}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          cache: "no-store",
+        }).then((res) => res.json() as Promise<LineupInfo[]>),
+      ),
+    );
+    const lineups = data.flat();
 
-    const data = await fetch(
-      "https://next.danvery.com/api/line-up?festivalDate=FIRST_DAY",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        cache: "no-store",
-      },
-    ).then((res) => res.text());
-    // const t = await getTranslations("LineupTile");
+    const t = await getTranslations("LineupTile");
 
     return (
       <div className="w-full">
-        {/* <TileHeader>
+        <TileHeader>
           <TileHeader.Head>{t("title")}</TileHeader.Head>
           <TileHeader.SeeAll href="/lineup">{t("seeAll")}</TileHeader.SeeAll>
-        </TileHeader> */}
+        </TileHeader>
         <div className="relative aspect-[3/4] w-full">
-          {/* <Carousel images={[data]} /> */}
-          {data}
+          <Carousel images={lineups} />
         </div>
       </div>
     );
