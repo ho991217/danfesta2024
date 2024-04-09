5d7818b61ef6d6116b0ae6f488011e485c914b78
이호연 <ho991217@kakao.com>
2024-04-09 16:42:59 +0900

    fix: post 수정

--- components/ticketing/form.tsx
+++ components/ticketing/form.tsx
@@ -1,16 +1,17 @@
-'use client';
+"use client";
 
-import { Form } from '../common';
-import { z } from 'zod';
-import APIError from '@/lib/utils/error/api-error';
-import { toast } from 'sonner';
-import { useRouter } from 'next/navigation';
-import { useLocale } from 'next-intl';
-import { API_ROUTES, API_URL, COOKIE_KEYS } from '@/constants';
-import { useCookies } from 'next-client-cookies';
+import { Form } from "../common";
+import { z } from "zod";
+import APIError from "@/lib/utils/error/api-error";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { useLocale } from "next-intl";
+import { API_ROUTES, API_URL, COOKIE_KEYS } from "@/constants";
+import { useCookies } from "next-client-cookies";
+import { post } from "@/api";
 
 const schema = z.object({
-  captchaValue: z.string().trim().min(1, { message: '필수 입력 사항입니다.' }),
+  captchaValue: z.string().trim().min(1, { message: "필수 입력 사항입니다." }),
 });
 
 export type TicketApplyRequest = {
@@ -24,42 +25,47 @@ type Schema = z.infer<typeof schema>;
 export default function TicketingForm({
   eventId,
   captchaKey,
-}: Omit<TicketApplyRequest, 'captchaValue'>) {
+}: Omit<TicketApplyRequest, "captchaValue">) {
   const router = useRouter();
   const locale = useLocale();
   const cookies = useCookies();
 
   const handleSubmit = async (v: Schema) => {
     const atk = cookies.get(COOKIE_KEYS.accessToken);
-    if (!atk) throw new Error('로그인이 필요합니다.');
+    if (!atk) throw new Error("로그인이 필요합니다.");
 
     try {
-      const res = await fetch(`${API_URL}${API_ROUTES.ticket.apply}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${atk}`,
-        },
-        body: JSON.stringify({
-          eventId,
-          captchaKey,
-          captchaValue: v.captchaValue,
-        }),
-      }).then((res) => res.json());
+      // const res = await fetch(`${API_URL}${API_ROUTES.ticket.apply}`, {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     Authorization: `Bearer ${atk}`,
+      //   },
+      //   body: JSON.stringify({
+      //     eventId,
+      //     captchaKey,
+      //     captchaValue: v.captchaValue,
+      //   }),
+      // }).then((res) => res.json());
+      const res = await post(API_ROUTES.ticket.apply, {
+        eventId,
+        captchaKey,
+        captchaValue: v.captchaValue,
+      });
 
-      if (
-        !res.ok &&
-        res?.message?.[0] ===
-          '자동입력 방지문자가 틀렸습니다. 키부터 다시 요청해주세요.'
-      )
-        throw new Error('캡차 값이 올바르지 않습니다.');
+      // if (
+      //   !res.ok &&
+      //   res?.message?.[0] ===
+      //     "자동입력 방지문자가 틀렸습니다. 키부터 다시 요청해주세요."
+      // )
+      //   throw new Error("캡차 값이 올바르지 않습니다.");
 
-      toast.success('신청이 완료되었습니다.');
+      toast.success("신청이 완료되었습니다.");
       router.push(`/${locale}/ticketing/${eventId}/result`);
     } catch (error) {
       const e = error as APIError;
       toast.error(e.message);
-      if (e.message !== '이미 신청했습니다.') router.refresh();
+      if (e.message !== "이미 신청했습니다.") router.refresh();
     }
   };
 
@@ -67,14 +73,14 @@ export default function TicketingForm({
     <Form
       onSubmit={handleSubmit}
       schema={schema}
-      className='flex gap-0'
-      validateOn='onSubmit'
+      className="flex gap-0"
+      validateOn="onSubmit"
     >
-      <Form.Text name='captchaValue' className='mb-4' />
-      <Form.Button type='submit' variant='filled' className='mb-2'>
+      <Form.Text name="captchaValue" className="mb-4" />
+      <Form.Button type="submit" variant="filled" className="mb-2">
         신청
       </Form.Button>
-      <span className='text-neutral-500 text-[10px]'>
+      <span className="text-[10px] text-neutral-500">
         이벤트 신청 시 개인정보 제공에 동의한 것으로 간주합니다.
       </span>
     </Form>
