48f4a45a8634a23192216081d48d3a5a92d07202
이호연 <ho991217@kakao.com>
2024-04-09 09:43:20 +0900

    버셀 툴 걷어내기

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,5 @@
 import NextTopLoader from 'nextjs-toploader';
 import { CookiesProvider } from 'next-client-cookies/server';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from '@/components/ui/sonner';
 
 type ProvidersProps = Readonly<{
@@ -11,8 +9,6 @@ type ProvidersProps = Readonly<{
 export default function Providers({ children }: ProvidersProps) {
   return (
     <CookiesProvider>
-      <SpeedInsights />
-      <Analytics />
       <NextTopLoader color='#0262E9' showSpinner={false} shadow={false} />
       <Toaster position='top-center' />
       {children}
