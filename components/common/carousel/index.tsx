57ceef1a997c1a86359e793b9b560f8316d49319
이호연 <ho991217@kakao.com>
2024-04-09 13:57:20 +0900

    qwer

--- components/common/carousel/index.tsx
+++ components/common/carousel/index.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import React from 'react';
-import useEmblaCarousel from 'embla-carousel-react';
-import { NextIntlClientProvider, useLocale } from 'next-intl';
-import Tile from './tile';
-import en from '../../../messages/en.json';
-import ko from '../../../messages/ko.json';
-import { type LineupInfo } from '@/app/[locale]/(back-nav)/lineup/page';
+import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { NextIntlClientProvider, useLocale } from "next-intl";
+import Tile from "./tile";
+import en from "../../../messages/en.json";
+import ko from "../../../messages/ko.json";
+import { type LineupInfo } from "@/app/[locale]/(back-nav)/lineup/page";
 
 type CarouselProps = {
   images: LineupInfo[];
@@ -18,21 +18,21 @@ export default function Carousel({ images }: CarouselProps) {
 
   return (
     <NextIntlClientProvider
-      messages={locale === 'en' ? en : ko}
-      timeZone='Asia/Seoul'
+      messages={locale === "en" ? en : ko}
+      timeZone="Asia/Seoul"
       formats={{
         dateTime: {
           short: {
-            day: 'numeric',
-            month: 'short',
-            year: 'numeric',
+            day: "numeric",
+            month: "short",
+            year: "numeric",
           },
         },
       }}
       locale={locale}
     >
-      <div className='absolute left-0 right-0' ref={emblaRef}>
-        <div className='flex gap-3'>
+      <div className="absolute left-0 right-0" ref={emblaRef}>
+        <div className="flex gap-3">
           {images.map((tile, index) => (
             <Tile key={index} {...tile} />
           ))}
--- components/home/lineup-tile.tsx
+++ components/home/lineup-tile.tsx
@@ -1,12 +1,12 @@
-import Carousel from '../common/carousel';
-import TileHeader from './tile-header';
-import { API_ROUTES, API_URL } from '@/constants';
-import { LineupInfo } from '@/app/[locale]/(back-nav)/lineup/page';
-import { getTranslations } from 'next-intl/server';
+import Carousel from "../common/carousel";
+import TileHeader from "./tile-header";
+import { API_ROUTES, API_URL } from "@/constants";
+import { LineupInfo } from "@/app/[locale]/(back-nav)/lineup/page";
+import { getTranslations } from "next-intl/server";
 
 export default async function LineupTile() {
   try {
-    const allDay = ['FIRST_DAY', 'SECOND_DAY', 'THIRD_DAY'] as const;
+    const allDay = ["FIRST_DAY", "SECOND_DAY", "THIRD_DAY"] as const;
     // const data = await Promise.all(
     //   allDay
     //     .map((day) =>
@@ -22,36 +22,32 @@ export default async function LineupTile() {
     // );
 
     const data = await fetch(
-      `${API_URL}${API_ROUTES.lineup.list('FIRST_DAY')}`,
+      `${API_URL}${API_ROUTES.lineup.list("FIRST_DAY")}`,
       {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        cache: 'no-store',
-      }
-    ).then((res) => res.json());
-    const t = await getTranslations('LineupTile');
+        cache: "no-store",
+      },
+    ).then((res) => res.text());
+    const t = await getTranslations("LineupTile");
 
     return (
-      <div className='w-full'>
+      <div className="w-full">
         <TileHeader>
-          <TileHeader.Head>{t('title')}</TileHeader.Head>
-          <TileHeader.SeeAll href='/lineup'>{t('seeAll')}</TileHeader.SeeAll>
+          <TileHeader.Head>{t("title")}</TileHeader.Head>
+          <TileHeader.SeeAll href="/lineup">{t("seeAll")}</TileHeader.SeeAll>
         </TileHeader>
-        <div className='w-full aspect-[3/4] relative'>
+        <div className="w-full aspect-[3/4] relative">
           {/* <Carousel images={[data]} /> */}
-          {data.map((tile: any, index: number) => (
-            <div key={index} className='w-full h-full'>
-              {tile.singer}
-            </div>
-          ))}
+          {data}
         </div>
       </div>
     );
   } catch (error) {
     const e = error as Error;
     console.error(error);
-    return <span className='w-full text-neutral-500'>{e.message}</span>;
+    return <span className="w-full text-neutral-500">{e.message}</span>;
   }
 }
