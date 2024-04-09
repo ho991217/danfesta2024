f39c02f05fc78b3f5f58904b234c73c0eec75e6d
이호연 <ho991217@kakao.com>
2024-04-10 02:42:06 +0900

    feat: 로그인 화면에 비디오 추가

--- app/[locale]/(back-nav)/login/page.tsx
+++ app/[locale]/(back-nav)/login/page.tsx
@@ -6,6 +6,7 @@ import { useAuth } from '@/hooks';
 import { Button, Form } from '@components/common';
 import APIError from '@lib/utils/error/api-error';
 import { useLocale } from 'next-intl';
+import Video from 'next-video';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { toast } from 'sonner';
@@ -32,43 +33,50 @@ export default function LoginPage() {
   };
 
   return (
-    <section className="flex w-full flex-col items-center justify-end px-5">
-      <div className="flex flex-col mb-14 mt-8 items-center">
-        {/* <div className="flex items-center w-full justify-center -translate-x-2">
-          <Logo width={75} />
-          <h1 className="text-3xl font-bold text-center">로그인</h1>
-        </div>
-        <span className="text-sm text-neutral-500 -translate-y-2">
-          빈공간 채우는 용도로 사용됩니다.
-        </span> */}
-        <video width="320" height="240" controls preload="none">
-          <source src="/videos/glass.mp4" type="video/mp4" />
-        </video>
+    <>
+      <div className="absolute top-0 -z-10 object-cover w-full">
+        <Video
+          className="object-cover w-full"
+          src={'/videos/glass.mp4'} // 비디오 파일 경로
+          autoPlay
+          muted
+          loop
+          playsInline
+          controls={false}
+        ></Video>
       </div>
-      <Form
-        className="flex flex-col gap-2"
-        onSubmit={onSubmit}
-        schema={authInfoSchema}
-      >
-        <Form.ID label="단국대학교 포털 아이디" placeholder="32123456" />
-        <Form.Password
-          label="단국대학교 포털 비밀번호"
-          placeholder="비밀번호"
-        />
-        <Form.Group className="mt-4">
-          <Form.Button type="submit" variant="bottom" isLoading={isLoading}>
-            로그인
-          </Form.Button>
-          <Button variant="transparent" animateOnClick>
-            <Link
-              href="/verify"
-              className="grid h-full w-full place-items-center"
-            >
-              회원가입
-            </Link>
-          </Button>
-        </Form.Group>
-      </Form>
-    </section>
+      <section className="flex w-full flex-col items-center justify-end px-5">
+        <div className="flex flex-col mb-24 mt-4 items-center">
+          <div className="flex items-center w-full justify-center">
+            <h1 className="text-3xl font-bold text-center">LOGIN</h1>
+          </div>
+          <span className="text-sm">서비스를 이용하시려면 로그인해주세요.</span>
+        </div>
+        <Form
+          className="flex flex-col gap-2"
+          onSubmit={onSubmit}
+          schema={authInfoSchema}
+        >
+          <Form.ID label="단국대학교 포털 아이디" placeholder="32123456" />
+          <Form.Password
+            label="단국대학교 포털 비밀번호"
+            placeholder="비밀번호"
+          />
+          <Form.Group className="mt-4">
+            <Form.Button type="submit" variant="bottom" isLoading={isLoading}>
+              로그인
+            </Form.Button>
+            <Button variant="transparent" animateOnClick>
+              <Link
+                href="/verify"
+                className="grid h-full w-full place-items-center"
+              >
+                회원가입
+              </Link>
+            </Button>
+          </Form.Group>
+        </Form>
+      </section>
+    </>
   );
 }
--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,13 +5,13 @@ export default function LocaleLayout({
 }) {
   return (
     <>
-      <div className='max-w-[600px] min-w-[320px] m-auto h-[100dvh] overflow-x-hidden lg:hidden scrollbar-hide'>
+      <div className="max-w-[600px] min-w-[320px] m-auto h-[100dvh] overflow-x-hidden lg:hidden scrollbar-hide relative">
         {children}
       </div>
       {/* <div className='max-w-[600px] min-w-[320px] m-auto px-5 pt-5 h-[100dvh] overflow-x-hidden lg:hidden scrollbar-hide'>
             {children}
           </div> */}
-      <div className='hidden lg:block lg:fixed lg:top-1/2 lg:left-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2'>
+      <div className="hidden lg:block lg:fixed lg:top-1/2 lg:left-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2">
         모바일 환경에서 접속해주세요.
       </div>
     </>
