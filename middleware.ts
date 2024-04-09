38f40932f48d4085a7c5ddbc41a808a3b157e601
이호연 <ho991217@kakao.com>
2024-04-09 16:03:07 +0900

    chore: updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,16 @@
-import createMiddleware from 'next-intl/middleware';
-import { NextRequest } from 'next/server';
-import { API_ROUTES, API_URL, COOKIE_KEYS } from './constants';
+import createMiddleware from "next-intl/middleware";
+import { NextRequest, NextResponse } from "next/server";
+import { API_ROUTES, API_URL, COOKIE_KEYS } from "./constants";
 
 const i18nMiddleware = createMiddleware({
-  locales: ['en', 'ko'],
-  defaultLocale: 'ko',
+  locales: ["en", "ko"],
+  defaultLocale: "ko",
   localeDetection: true,
 });
 
 const middleware = async (request: NextRequest) => {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("Content-Security-Policy", "upgrade-insecure-requests;"); // http요청을 https로 변경
   // const atk = request.cookies.get(COOKIE_KEYS.accessToken);
 
   // if (request.nextUrl.pathname.includes('/ticketing')) {
@@ -25,11 +27,16 @@ const middleware = async (request: NextRequest) => {
   //   }
   // }
 
-  return i18nMiddleware(request);
+  const response = i18nMiddleware(request).headers.set(
+    "Content-Security-Policy",
+    "upgrade-insecure-requests;",
+  );
+
+  return response;
 };
 
 export const config = {
-  matcher: ['/', '/(ko|en)/:path*'],
+  matcher: ["/", "/(ko|en)/:path*"],
 };
 
 export default middleware;
