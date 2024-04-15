bdfd6c8353ac086ab273841fef1c32eba8f1ad9a
이호연 <ho991217@kakao.com>
2024-04-15 16:56:54 +0900

    feat: 비밀번호 리셋 완성

--- app/[locale]/(back-nav)/find-my/id/complete/page.tsx
+++ app/[locale]/(back-nav)/find-my/id/complete/page.tsx
@@ -4,11 +4,11 @@ import { ROUTES } from '@/constants';
 export default function FindMyIdCompletePage() {
   return (
     <>
-      <header className="flex flex-col gap-2 mb-10">
+      <header className="flex flex-col gap-2 mt-8">
         <h1 className="text-[27px] font-bold">
           등록하신 번호로 <br /> 아이디를 전송하였습니다.
         </h1>
-        <span className="text-base flex">
+        <span className="text-base flex text-neutral-500">
           전송받은 아이디(학번)으로 <br /> 로그인 해 주세요.
         </span>
       </header>
--- app/[locale]/(back-nav)/find-my/password/complete/page.tsx
+++ app/[locale]/(back-nav)/find-my/password/complete/page.tsx
@@ -1,3 +1,23 @@
+import { Button, Link } from '@/components/common';
+import { ROUTES } from '@/constants';
+
 export default function FindMyPasswordCompletePage() {
-  return <div>done!</div>
+  return (
+    <>
+      <header className="flex flex-col gap-2 mt-8">
+        <h1 className="text-[27px] font-bold">비밀번호가 변경되었습니다.</h1>
+        <span className="text-base flex text-neutral-500">
+          변경하신 비밀번호로 로그인 해 주세요.
+        </span>
+      </header>
+      <Button variant="bottom">
+        <Link
+          href={ROUTES.login}
+          className="w-full h-full flex items-center justify-center"
+        >
+          로그인하러 가기
+        </Link>
+      </Button>
+    </>
+  );
 }
