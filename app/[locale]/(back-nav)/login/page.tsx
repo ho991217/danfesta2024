744da0a1dee9908151790fef708476f23ca6a6cf
이호연 <ho991217@kakao.com>
2024-04-15 09:39:35 +0900

    chore: 비디오 제거

--- app/[locale]/(back-nav)/login/page.tsx
+++ app/[locale]/(back-nav)/login/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import { Link } from '@/components/common';
 import { useAuth } from '@/hooks';
-// import Glass from '@/public/videos/glass.mp4';
-import { Button, Form } from '@components/common';
+import { Button, Form, Link } from '@components/common';
 import APIError from '@lib/utils/error/api-error';
 import { useLocale } from 'next-intl';
-import Video from 'next-video';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { toast } from 'sonner';
@@ -14,71 +11,60 @@ import { toast } from 'sonner';
 import { AuthInfoSchema, authInfoSchema } from './schema';
 
 export default function LoginPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const locale = useLocale();
-  const { login } = useAuth();
-  const router = useRouter();
+   const [isLoading, setIsLoading] = useState(false);
+   const locale = useLocale();
+   const { login } = useAuth();
+   const router = useRouter();
 
-  const onSubmit = async (data: AuthInfoSchema) => {
-    try {
-      setIsLoading(true);
-      await login(data);
-      router.push(`/${locale}/`);
-    } catch (error) {
-      const e = error as APIError;
-      toast.error(e.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+   const onSubmit = async (data: AuthInfoSchema) => {
+      try {
+         setIsLoading(true);
+         await login(data);
+         router.push(`/${locale}/`);
+      } catch (error) {
+         const e = error as APIError;
+         toast.error(e.message);
+      } finally {
+         setIsLoading(false);
+      }
+   };
 
-  return (
-    <>
-      <div className="absolute top-0 left-0 -z-10">
-        <Video
-          className="object-cover w-full"
-          src={'/videos/glass.mp4'}
-          autoPlay
-          muted
-          loop
-          playsInline
-          controls={false}
-        ></Video>
-        <div className="flex flex-col items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute min-w-[300px]">
-          <div className="flex items-center w-full justify-center">
-            <h1 className="text-3xl font-bold text-center text-white">LOGIN</h1>
-          </div>
-          <span className="text-sm text-white">
-            서비스를 이용하시려면 로그인해주세요.
-          </span>
-        </div>
-      </div>
-      <section className="w-full mt-[calc(100vw*0.5625-72px+1rem)]">
-        <Form
-          className="flex flex-col gap-2 px-5 mt-3 z-50"
-          onSubmit={onSubmit}
-          schema={authInfoSchema}
-        >
-          <Form.ID label="단국대학교 포털 아이디" placeholder="32123456" />
-          <Form.Password
-            label="단국대학교 포털 비밀번호"
-            placeholder="비밀번호"
-          />
-          <Form.Group className="mt-4">
-            <Form.Button type="submit" variant="bottom" isLoading={isLoading}>
-              로그인
-            </Form.Button>
-            <Button variant="transparent" animateOnClick>
-              <Link
-                href="/verify"
-                className="grid h-full w-full place-items-center"
-              >
-                회원가입
-              </Link>
-            </Button>
-          </Form.Group>
-        </Form>
+   return (
+      <section className="w-full">
+         <div className="flex flex-col items-center w-full pt-6 pb-10">
+            <h1 className="text-3xl font-bold text-center">LOGIN</h1>
+            <span className="text-sm">
+               서비스를 이용하시려면 로그인해주세요.
+            </span>
+         </div>
+         <Form
+            className="flex flex-col gap-2 px-5 mt-3 z-50"
+            onSubmit={onSubmit}
+            schema={authInfoSchema}
+         >
+            <Form.ID label="단국대학교 포털 아이디" placeholder="32123456" />
+            <Form.Password
+               label="단국대학교 포털 비밀번호"
+               placeholder="비밀번호"
+            />
+            <Form.Group className="mt-4">
+               <Form.Button
+                  type="submit"
+                  variant="bottom"
+                  isLoading={isLoading}
+               >
+                  로그인
+               </Form.Button>
+               <Button variant="transparent" animateOnClick>
+                  <Link
+                     href="/verify"
+                     className="grid h-full w-full place-items-center"
+                  >
+                     회원가입
+                  </Link>
+               </Button>
+            </Form.Group>
+         </Form>
       </section>
-    </>
-  );
+   );
 }
--- hooks/use-auth.ts
+++ hooks/use-auth.ts
@@ -11,85 +11,88 @@ import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
 export default function useAuth() {
-  const [userInfo, setUserInfo] = useState<User | null>(null);
-  const { isLoggedIn, setIsLoggedIn } = useAuthStore();
-  const cookies = useCookies();
-  const router = useRouter();
+   const [userInfo, setUserInfo] = useState<User | null>(null);
+   const { isLoggedIn, setIsLoggedIn } = useAuthStore();
+   const cookies = useCookies();
+   const router = useRouter();
 
-  const getUserInfo = async () => {
-    try {
-      const accessToken = cookies.get(COOKIE_KEYS.accessToken);
-      const refreshToken = cookies.get(COOKIE_KEYS.refreshToken);
+   const getUserInfo = async () => {
+      try {
+         const accessToken = cookies.get(COOKIE_KEYS.accessToken);
+         const refreshToken = cookies.get(COOKIE_KEYS.refreshToken);
 
-      if (!accessToken || !refreshToken) {
-        throw new Error('토큰이 없습니다.');
+         if (!accessToken || !refreshToken) {
+            throw new Error('토큰이 없습니다.');
+         }
+
+         const res = await fetch(`${API_URL}${API_ROUTES.user.me}`, {
+            headers: {
+               Authorization: `Bearer ${accessToken}`,
+            },
+         }).then((res) => res.json() as Promise<User>);
+
+         setUserInfo(res);
+      } catch (error) {
+         throw new Error('사용자 정보를 가져오는데 실패했습니다.');
       }
+   };
 
-      const res = await fetch(`${API_URL}${API_ROUTES.user.me}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }).then((res) => res.json() as Promise<User>);
+   const checkLogin = () => {
+      getUserInfo()
+         .then(() => setIsLoggedIn(true))
+         .catch(() => setIsLoggedIn(false));
+   };
 
-      setUserInfo(res);
-    } catch (error) {
-      throw new Error('사용자 정보를 가져오는데 실패했습니다.');
-    }
-  };
+   // const checkStudentVerified = async () => {
 
-  const checkLogin = () => {
-    getUserInfo()
-      .then(() => setIsLoggedIn(true))
-      .catch(() => setIsLoggedIn(false));
-  };
+   useEffect(() => {
+      if (isLoggedIn) return;
+      checkLogin();
+   }, []);
 
-  useEffect(() => {
-    if (isLoggedIn) return;
-    checkLogin();
-  }, []);
+   const login = async (req: AuthInfoSchema, redirect?: string) => {
+      try {
+         const res = await post<AuthInfoSchema, AuthTokens>(
+            API_ROUTES.user.login,
+            req,
+         );
 
-  const login = async (req: AuthInfoSchema, redirect?: string) => {
-    try {
-      const res = await post<AuthInfoSchema, AuthTokens>(
-        API_ROUTES.user.login,
-        req,
-      );
+         if (!res.accessToken || !res.refreshToken)
+            throw new ApiError(res as any);
+         cookies.set(COOKIE_KEYS.accessToken, res.accessToken);
+         cookies.set(COOKIE_KEYS.refreshToken, res.refreshToken);
 
-      if (!res.accessToken || !res.refreshToken) throw new ApiError(res as any);
-      cookies.set(COOKIE_KEYS.accessToken, res.accessToken);
-      cookies.set(COOKIE_KEYS.refreshToken, res.refreshToken);
+         setIsLoggedIn(true);
+         toast.info('로그인 되었습니다.', {
+            duration: 36000,
+            action: {
+               label: '확인',
+               onClick: () => {
+                  router.push('/ko/verify');
+               },
+            },
+         });
+         router.push(redirect || '/');
+      } catch (error) {
+         const e = error as Error;
+         setIsLoggedIn(false);
+         throw e;
+      }
+   };
 
-      setIsLoggedIn(true);
-      toast.info('로그인 되었습니다.', {
-        duration: 36000,
-        action: {
-          label: '확인',
-          onClick: () => {
-            router.push('/ko/verify');
-          },
-        },
-      });
-      router.push(redirect || '/');
-    } catch (error) {
-      const e = error as Error;
+   const logout = () => {
+      cookies.remove(COOKIE_KEYS.accessToken);
+      cookies.remove(COOKIE_KEYS.refreshToken);
+      setUserInfo(null);
+      toast.info('로그아웃되었습니다.');
       setIsLoggedIn(false);
-      throw e;
-    }
-  };
+      router.push('/');
+   };
 
-  const logout = () => {
-    cookies.remove(COOKIE_KEYS.accessToken);
-    cookies.remove(COOKIE_KEYS.refreshToken);
-    setUserInfo(null);
-    toast.info('로그아웃되었습니다.');
-    setIsLoggedIn(false);
-    router.push('/');
-  };
-
-  return {
-    userInfo,
-    isLoggedIn,
-    login,
-    logout,
-  };
+   return {
+      userInfo,
+      isLoggedIn,
+      login,
+      logout,
+   };
 }
