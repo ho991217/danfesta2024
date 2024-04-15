6eecee49181060cad26535b423df2eee5bfb8788
이호연 <ho991217@kakao.com>
2024-04-15 10:04:22 +0900

    chore:

--- components/home/lineup-tile.tsx
+++ components/home/lineup-tile.tsx
@@ -1,25 +1,26 @@
-import { get } from "@/api";
-import Carousel from "../common/carousel";
-import TileHeader from "./tile-header";
-import { LineupInfo } from "@/app/[locale]/(back-nav)/lineup/page";
-import { getTranslations } from "next-intl/server";
-import { API_ROUTES } from "@/constants";
+import { get } from '@/api';
+import { API_ROUTES } from '@/constants';
+import { LineupInfo } from '@app/[locale]/(back-nav)/lineup/page';
+import { getTranslations } from 'next-intl/server';
+
+import Carousel from '../common/carousel';
+import TileHeader from './tile-header';
 
 export default async function LineupTile() {
   try {
-    const allDay = ["FIRST_DAY", "SECOND_DAY", "THIRD_DAY"] as const;
+    const allDay = ['FIRST_DAY', 'SECOND_DAY', 'THIRD_DAY'] as const;
     const data = await Promise.all(
       allDay.map((day) => get<LineupInfo[]>(API_ROUTES.lineup.list(day))),
     );
     const lineups = data.flat();
 
-    const t = await getTranslations("LineupTile");
+    const t = await getTranslations('LineupTile');
 
     return (
       <div className="w-full">
         <TileHeader>
-          <TileHeader.Head>{t("title")}</TileHeader.Head>
-          <TileHeader.SeeAll href="/lineup">{t("seeAll")}</TileHeader.SeeAll>
+          <TileHeader.Head>{t('title')}</TileHeader.Head>
+          <TileHeader.SeeAll href="/lineup">{t('seeAll')}</TileHeader.SeeAll>
         </TileHeader>
         <div className="relative aspect-[3/4] w-full">
           <Carousel lineups={lineups} />
