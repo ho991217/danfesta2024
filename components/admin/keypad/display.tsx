d4ee76026b90eca00f462002b490bc2bfecdbc1a
이호연 <ho991217@kakao.com>
2024-04-19 17:23:14 +0900

    feat: 애니메이션 추가

--- components/admin/keypad/display.tsx
+++ components/admin/keypad/display.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { PropsWithChildren } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 type DisplayProps = {
   value: string;
@@ -11,16 +11,29 @@ export default function Display({ value, slot }: DisplayProps) {
   return (
     <div className="flex gap-2 w-full px-8">
       {Array.from({ length: slot }).map((_, i) => (
-        <Block key={i}>{value[i] || ' '}</Block>
+        <Block key={i} value={value[i]} />
       ))}
     </div>
   );
 }
 
-function Block({ children }: PropsWithChildren) {
+function Block({ value }: { value: string }) {
   return (
-    <span className="aspect-[3/4] w-full rounded-md bg-neutral-200 dark:bg-neutral-800 flex items-center justify-center text-3xl">
-      {children}
-    </span>
+    <div className="aspect-[3/4] w-full rounded-md bg-neutral-200 dark:bg-neutral-800 flex items-center justify-center text-3xl">
+      <AnimatePresence>
+        {value !== undefined && (
+          <motion.div
+            layout
+            className="w-full h-full flex items-center justify-center"
+            initial={{ opacity: 0, scale: 0.75, y: 10 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.75, y: -10 }}
+            transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+          >
+            {value}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
