5a878416c2d54a77940fc3d9715f5f70c4f9f1d2
이호연 <ho991217@kakao.com>
2024-04-24 00:37:47 +0900

    asd

--- app/[locale]/(back-nav)/stamp/page.tsx
+++ app/[locale]/(back-nav)/stamp/page.tsx
@@ -1,27 +1,10 @@
-import Stamp1 from '@/public/images/stamp/스탬프판_문체-02.webp';
-import Stamp2 from '@/public/images/stamp/스탬프판_문체-03.webp';
-import Stamp3 from '@/public/images/stamp/스탬프판_문체-04.webp';
-import Stamp4 from '@/public/images/stamp/스탬프판_문체-05.webp';
-import Stamp5 from '@/public/images/stamp/스탬프판_문체-06.webp';
-import Stamp6 from '@/public/images/stamp/스탬프판_문체-07.webp';
-import Stamp7 from '@/public/images/stamp/스탬프판_문체-08.webp';
-import StampBoard from '@/public/images/stamp/스탬프판_문체_배경.webp';
-import Image from 'next/image';
 
-const stamps = [
-  Stamp1,
-  Stamp2,
-  Stamp3,
-  Stamp4,
-  Stamp5,
-  Stamp6,
-  Stamp7,
-] as const;
+import Image from 'next/image';
 
 export default async function StampPage() {
   return (
     <div>
-      <Image src={StampBoard} alt="stamp board" />
+      {/* <Image src={StampBoard} alt="stamp board" /> */}
     </div>
   );
 }
