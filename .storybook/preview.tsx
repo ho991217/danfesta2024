aa369271e8a572dd94b3031e7a23eba6d9ee1353
JUNSEOK <kd02109@gmail.com>
2024-05-01 17:50:25 +0900

    feat : Ticketing storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,23 @@
 import { withThemeByClassName } from '@storybook/addon-themes';
 import type { Preview } from '@storybook/react';
 import { IntlProvider } from 'next-intl';
+import {
+  AppRouterContext,
+  AppRouterInstance,
+} from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import React from 'react';
 import { Toaster } from 'sonner';
 
 import '../app/globals.css';
+
+const mockRouter: AppRouterInstance = {
+  push: () => Promise.resolve(true),
+  prefetch: () => Promise.resolve(),
+  replace: () => Promise.resolve(true),
+  back: () => {},
+  forward: () => Promise.resolve(true),
+  refresh: () => Promise.resolve(),
+};
 
 const preview: Preview = {
   parameters: {
@@ -19,18 +32,20 @@ const preview: Preview = {
   decorators: [
     (Story) => {
       return (
-        <IntlProvider locale="ko">
-          <div
-            style={{
-              paddingTop: '20 10',
-              height: '80dvh',
-              width: '50dvw',
-            }}
-          >
-            <Toaster position="top-center" />
-            <Story />
-          </div>
-        </IntlProvider>
+        <AppRouterContext.Provider value={mockRouter}>
+          <IntlProvider locale="ko">
+            <div
+              style={{
+                paddingTop: '20 10',
+                height: '80dvh',
+                width: '50dvw',
+              }}
+            >
+              <Toaster position="top-center" />
+              <Story />
+            </div>
+          </IntlProvider>
+        </AppRouterContext.Provider>
       );
     },
   ],
---
+++ stories/components/ticketing/Form.stories.tsx
@@ -0,0 +1,26 @@
+import { Form } from '@/components/ticketing';
+//import { useClientSideToken } from '@/hooks';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Form> = {
+  title: 'COMPONENTS/TICKETING/Form',
+  component: Form,
+  argTypes: {},
+  decorators: [
+    (Story) => {
+      return <Story />;
+    },
+  ],
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Template = StoryObj<typeof Form>;
+
+export const Default: Template = {
+  args: {
+    eventId: '123',
+    captchaKey: '123',
+  },
+};
