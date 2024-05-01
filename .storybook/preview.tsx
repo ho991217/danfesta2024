c206589c5e4c8170e19306bf81b250718612c3c8
JUNSEOK <kd02109@gmail.com>
2024-05-01 13:42:33 +0900

    dep : storybook 설치

---
+++ .storybook/preview.tsx
@@ -0,0 +1,43 @@
+import { withThemeByClassName } from '@storybook/addon-themes';
+import type { Preview } from '@storybook/react';
+import { IntlProvider } from 'next-intl';
+import React from 'react';
+import { Toaster } from 'sonner';
+
+import '../app/globals.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+      viewport: { defaultViewport: 'mobile1' },
+    },
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <IntlProvider locale="ko">
+          <div style={{ paddingTop: '20 10' }}>
+            <Toaster position="top-center" />
+            <Story />
+          </div>
+        </IntlProvider>
+      );
+    },
+  ],
+};
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+  }),
+];
+
+export default preview;
