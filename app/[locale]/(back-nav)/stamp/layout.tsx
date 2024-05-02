edfaea2f2d2e38923b614df5f67de7b70f1306ae
이호연 <ho991217@kakao.com>
2024-05-02 16:34:45 +0900

    feat: 스탬프 판 추가

--- app/[locale]/(back-nav)/stamp/layout.tsx
+++ app/[locale]/(back-nav)/stamp/layout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react';
 
 export default function Layout({ children }: PropsWithChildren) {
-  return <section className="flex flex-col px-5">{children}</section>;
+  return <section className="flex flex-col">{children}</section>;
 }
