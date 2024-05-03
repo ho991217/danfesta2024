55efd82882f6014635c8db5dbf9a08bad57c02b5
이호연 <ho991217@kakao.com>
2024-05-03 14:07:53 +0900

    feat: 티켓 번호 확인 로직 구현

--- components/my-tickets/ticket.tsx
+++ components/my-tickets/ticket.tsx
@@ -1,5 +1,8 @@
+import Logo from '@/public/icons/logo-white.svg';
+import Glass from '@/public/images/glass.jpeg';
 import { TicketInfo } from '@app/[locale]/(back-nav)/my-tickets/action';
-import { FiCalendar } from 'react-icons/fi';
+import Image from 'next/image';
+import { FiClock } from 'react-icons/fi';
 
 import {
   AccordionContent,
@@ -13,25 +16,39 @@ export default async function Ticket({ id, event, issued }: TicketInfo) {
   return (
     <AccordionItem
       value={String(id)}
-      className="flex flex-col items-center justify-between border-[1px] border-neutral-300 w-full rounded-2xl dark:border-neutral-800"
+      className="flex flex-col items-center justify-between border-[1px] border-neutral-300 w-full rounded-2xl dark:border-neutral-800 overflow-hidden shadow-xl"
     >
-      <div className="w-full flex items-center justify-end pr-3 pt-1">
-        <TicketTool ticketId={id} />
+      <div className="bg-black relative h-[10rem] w-full">
+        <div className="w-full flex items-center justify-end pr-3 pt-1 absolute top-0 z-50">
+          <TicketTool ticketId={id} />
+        </div>
+        <div className="absolute left-5 top-1/2 -translate-y-1/2 flex items-center gap-2">
+          <Logo size={10} className="h-5 w-7 animate-in" />
+          <h3 className="text-neutral-100 font-bold text-2xl">DANFESTA</h3>
+        </div>
+        <Image
+          src={Glass}
+          alt="유리 이미지"
+          className="mix-blend-lighten absolute -right-20 -top-8 scale-125 opacity-50"
+        />
       </div>
-      <div className="px-5 pb-5 flex w-full flex-col gap-2">
-        <h4 className="text-base font-bold tex-start w-full">{event.name}</h4>
+      <div className="pt-5 px-5 flex w-full flex-col gap-2">
+        <div>
+          <span className="text-xs text-neutral-500">2024 단페스타</span>
+          <h4 className="text-2xl font-bold w-full">{event.name}</h4>
+        </div>
         <div className="flex items-center gap-2">
-          <FiCalendar color="#929497" />
+          <FiClock color="#929497" />
           <span className="text-sm text-[#929497]">
-            {new Date(event.startAt).toLocaleDateString()}
-          </span>
-          <span className="text-sm text-[#929497]">~</span>
-          <span className="text-sm text-[#929497]">
-            {new Date(event.endAt).toLocaleDateString()}
+            6시부터 입장 가능
           </span>
         </div>
       </div>
-      {!issued && (
+      {issued ? (
+        <span className="w-full pb-5 pt-4 flex items-center justify-center text-neutral-300 dark:text-neutral-700">
+          발급 완료된 티켓입니다.
+        </span>
+      ) : (
         <>
           <AccordionTrigger className="flex items-center">
             <span className="w-full rounded-b-xl px-5 text-center text-primary">
