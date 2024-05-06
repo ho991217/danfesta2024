dfe2a43a8fa4b58d4ffbc4fffe46fc3676e81741
이호연 <ho991217@kakao.com>
2024-05-06 19:13:58 +0900

    feat: 재학생 재인증 대표 화면 추가

--- app/[locale]/(root-nav)/page.tsx
+++ app/[locale]/(root-nav)/page.tsx
@@ -1,33 +1,18 @@
-import { getIsLoggedIn, getIsVerified } from '@api/.';
-import { AdBanner, Link } from '@components/common';
-import { LineupTile, MenuTiles, NoticeTile } from '@components/home';
+import { AdBanner } from '@components/common';
+import {
+  LineupTile,
+  MenuTiles,
+  NeedReverificationTile,
+} from '@components/home';
 
 import { getAllLineupInfo } from '../(back-nav)/lineup/actions';
 
 export default async function Home() {
-  const isVerified = await getIsVerified();
-  const isLoggedIn = await getIsLoggedIn();
   const lineups = await getAllLineupInfo();
 
   return (
     <div className="mb-20 flex flex-col gap-4 px-5 lg:mx-auto lg:max-w-full lg:px-10 lg:gap-8">
-      {isLoggedIn && !isVerified && (
-        <NoticeTile>
-          <span>티켓팅 서비스를 이용하려면 재학생 인증이 필요해요.</span>
-          <div className="w-full flex items-center justify-end mt-4">
-            <Link
-              className="h-auto w-auto p-2 text-primary font-medium dark:text-primary"
-              href={{
-                pathname: '/verify',
-                query: { reverify: 'true' },
-              }}
-              variant="transparent"
-            >
-              재학생 인증하기
-            </Link>
-          </div>
-        </NoticeTile>
-      )}
+      <NeedReverificationTile />
       <LineupTile lineups={lineups} />
       <AdBanner />
       <MenuTiles />
--- app/api/get-is-verified.ts
+++ app/api/get-is-verified.ts
@@ -1,17 +1,17 @@
 'use server';
 
-import { User, get, getServerSideToken } from '@/app/api';
-import { API_ROUTES, COOKIE_KEYS } from '@/app/lib/constants';
+import { User, get, getServerSideToken } from '@api/.';
+import { API_ROUTES, COOKIE_KEYS } from '@lib/constants';
 import { cookies } from 'next/headers';
 
 const getIsVerified = async () => {
   try {
     const verified = cookies().get(COOKIE_KEYS.verified)?.value;
-    const token = await getServerSideToken();
 
     if (verified === undefined || !Boolean(verified)) {
       const { dkuChecked } = await get<User>(API_ROUTES.user.me, {
-        token,
+        token: await getServerSideToken(),
+        cache: false,
       });
 
       return dkuChecked;
