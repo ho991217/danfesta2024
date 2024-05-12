a30dd59ae78c7e33f4e2eb7ae697aa1a75c29065
이호연 <ho991217@kakao.com>
2024-05-12 19:53:58 +0900

    feat: ad 배너 추가

--- app/components/common/ad-banner/index.tsx
+++ app/components/common/ad-banner/index.tsx
@@ -1,6 +1,5 @@
 import { get } from '@api/.';
 
-
 import Carousel from '../carousel';
 import Block from './block';
 
@@ -21,12 +20,14 @@ export default async function AdBanner() {
   const ads = await get<Ad[]>('/banner');
 
   return (
-    <div className="w-full relative px-5 aspect-[728/140]">
-      <Carousel loop autoplay>
-        {ads.map((ad) => (
-          <Block key={ad.id} ad={ad} />
-        ))}
-      </Carousel>
-    </div>
+    ads.length > 0 && (
+      <div className="w-full relative px-5 aspect-[728/140] lg:aspect-auto">
+        <Carousel loop autoplay>
+          {ads.map((ad) => (
+            <Block key={ad.id} ad={ad} />
+          ))}
+        </Carousel>
+      </div>
+    )
   );
 }
