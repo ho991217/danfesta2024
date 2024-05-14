fd3fe83a1dc199c62d140749faa9b43510cfef27
이호연 <ho991217@kakao.com>
2024-05-14 16:38:48 +0900

    feat: 날짜 정적으로 수정

--- app/components/lineup/date-selector/index.tsx
+++ app/components/lineup/date-selector/index.tsx
@@ -23,20 +23,24 @@ export default async function DateSelector({
   const beforeOneDay = new Date(year, month, day - 1);
   const afterOneDay = new Date(year, month, day + 2);
 
-  console.log('days', days);
-
   return (
     <div className="w-full h-16 flex items-center justify-between">
       <DateBlock date={beforeTwoDay} disabled />
       <DateBlock date={beforeOneDay} disabled />
-      {days.map((day, index) => (
-        <DateBlock
-          key={index}
-          date={new Date(day.performanceTime)}
-          festivalDate={day.festivalDate}
-          selected={day.festivalDate === selectedDay}
-        />
-      ))}
+      {/* {days.map((day, index) => ( */}
+      <DateBlock
+        // key={index}
+        date={new Date('2024-05-22')}
+        festivalDate={'FIRST_DAY'}
+        selected={selectedDay === 'FIRST_DAY'}
+      />
+      <DateBlock
+        // key={index}
+        date={new Date('2024-05-23')}
+        festivalDate={'SECOND_DAY'}
+        selected={selectedDay === 'SECOND_DAY'}
+      />
+      {/* ))} */}
       <DateBlock date={afterOneDay} disabled />
     </div>
   );
