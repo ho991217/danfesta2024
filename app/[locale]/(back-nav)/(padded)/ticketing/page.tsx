0d18b9e286ff305d8dc3f1a3e074e1b4309715d6
이호연 <ho991217@kakao.com>
2024-05-17 14:43:03 +0900

    feat: 지난 티켓 이벤트 보기 추가

--- app/[locale]/(back-nav)/(padded)/ticketing/page.tsx
+++ app/[locale]/(back-nav)/(padded)/ticketing/page.tsx
@@ -1,16 +1,26 @@
-import { Card } from '@components/ticketing';
+import { IntlProvider } from '@components/common';
+import { Card, ShowAll } from '@components/ticketing';
 import { SearchParams } from '@lib/types';
+import dayjs from 'dayjs';
 import { getTranslations } from 'next-intl/server';
 
 import { getEvents } from './action';
 
 export default async function TicketingPage({
-  searchParams: { show },
+  searchParams: { show = 'active' },
 }: SearchParams<{ show: 'all' | 'active' }>) {
-  const events = await getEvents();
+  const all = await getEvents();
+  const events = all.filter((e) =>
+    show === 'active'
+      ? dayjs(e.from).isBefore(dayjs()) && dayjs(e.to).isAfter(dayjs())
+      : true,
+  );
   const t = await getTranslations('Ticketing.page');
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 mb-20">
+      <IntlProvider>
+        <ShowAll show={show} />
+      </IntlProvider>
       {events.length > 0 ? (
         events.map((e) => <Card key={e.id} {...e} />)
       ) : (
--- app/components/ticketing/index.ts
+++ app/components/ticketing/index.ts
@@ -1,3 +1,4 @@
 export { default as Card } from './card';
 export { default as Form } from './form';
 export { default as RefetchButton } from './refetch-button';
+export { default as ShowAll } from './show-all';
